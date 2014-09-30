173449d2041a09920fe3ab8957970cf8ed51582c
Peter Skipper <peter.skipper@gmail.com>
2014-09-29 17:07:04 -0700

    Added basic parser. --help works now.

--- snippets.py
+++ snippets.py
@@ -1,5 +1,7 @@
 import logging
 import csv
+import argparse
+import sys
 
 #Set the log output file, and the log level
 logging.basicConfig(filename="output.log",level=logging.DEBUG)
@@ -15,3 +17,19 @@ def put(name, snippet, filename):
     logging.debug("Write successful")
     return name, snippet
 
+def make_parser():
+    """Construct the command line parser"""
+    logging.info("Constructing parser")
+    description = "Store and retrieve snippets of text"
+    parser = argparse.ArgumentParser(description=description)
+
+    return parser
+
+def main():
+    """Main function"""
+    logging.info("Starting snippets")
+    parser = make_parser()
+    arguments = parser.parse_args(sys.argv[1:])
+
+if __name__ == '__main__':
+    main()
